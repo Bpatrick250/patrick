90bb5e30bc167f80de2914485edca904ee4bde59
patrickirakiza <patrickbigizi@gmail.com>
2025-02-11 15:53:10 +0200

    Add files via upload

---
+++ arry.js
@@ -0,0 +1,17 @@
+let fruits = ["apple", "banana", "cherry"];
+
+
+console.log(fruits[0]);  
+console.log(fruits[2]); 
+
+
+console.log(fruits);  
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers[0]);
+console.log(numbers[2]);  
+
+numbers[1] = 10;
+console.log(numbers);  
